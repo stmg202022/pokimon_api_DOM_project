2971e2dcdd0919beaf1b89899e30bea9ef20b636
samsontmg <stmg202023@gmail.com>
2023-12-28 16:00:53 +0545

    added: body dark theme toggle

--- theme__button.js
+++ theme__button.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", async () => {
   const themeBtn = document.getElementById("themeBtn");
   const lightMode = document.getElementById("whiteCircle");
   const darkMode = document.getElementById("blackCircle");
+  const body = document.body;
 
   themeBtn.addEventListener("click", async () => {
     lightMode.classList.toggle("white__circle__toggle");
     darkMode.classList.toggle("black__circle__toggle");
     themeBtn.classList.toggle("theme__btn__toggle");
+    body.classList.toggle("body__toggle");
   });
 });
